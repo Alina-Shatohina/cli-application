340b7a9593e27713de043127630ea367e659f4f7
Andrew-Shatokhin <114595292+Alina-Shatohina@users.noreply.github.com>
2023-05-06 21:05:18 +0300

    add commander

--- contacts.js
+++ contacts.js
@@ -3,9 +3,8 @@ const path = require("path");
 const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
-console.log(contactsPath);
-    
 
+    
 async function listContacts() {
     const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 const contacts = require("./contacts");
-console.log(contacts);
+
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
-      // break;
+      return console.table(allContacts);
+     
 
     case "get":
         const oneContact = await contacts.getContactById(id);
         return console.log(oneContact);
-      // break;
+    
 
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
       return console.log(newContact);
-      // break;
+     
 
     case "remove":
       const removeContact = await contacts.removeContact(id);
       return console.log(removeContact);
-      // break;
+     
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 }
 
-// invokeAction({ action: "list" });
-// invokeAction({ action: "get", id: "qdggE76Jtbfd9eWJHrssH" });
-// invokeAction({ action: "add", name: "Misha", email: "Misha@gmail.com", phone: "(068)555-5555" });
-invokeAction({ action: "remove", id: "kXQ4Ym83q2QT5SrA3uFyo"});
+invokeAction(argv);
